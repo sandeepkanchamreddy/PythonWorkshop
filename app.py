e7459bfc604877a0edcbadf56303daf93463f1ca
sandeepkanchamreddy <133360778+sandeepkanchamreddy@users.noreply.github.com>
2025-05-21 16:39:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 """Python Flask app for workshop registration and email confirmation."""
+import os
 import smtplib
 from email.message import EmailMessage
 from flask import Flask, render_template, request
@@ -73,4 +74,5 @@ Sandeep Kumar Kanchamreddy
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Render will set the PORT env variable
+    app.run(host="0.0.0.0", port=port) 
